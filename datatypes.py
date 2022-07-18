4cd239002e863953739542683df56a925b12b52f
nic ledger <nicledgerallmail@gmail.com>
2022-07-18 21:44:10 +0100

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,56 @@
+import os
+os.system('clear')
+####################
+#variables declared
+'''
+first_name = "John"
+#print(first_name)
+'''
+###################
+###################
+'''
+first_name = "John"
+#print(first_name)
+#first_name = "Bob"
+#print(first_name)
+'''
+###################
+#####################################
+#prints out hello world to the screen
+#prints variable
+#print("Hello World!")
+#####################################
+
+####################################
+# DATA TYPES
+# Strings
+# Numbers
+# Lists
+# Tuples
+# Dictionaries
+# Boolean
+####################################
+#string
+first_name = 'john'
+#numbers
+age = 41
+'''
+#lists
+names = ["John","Bob","Mary"]
+print(names[0])
+#tuples
+names = ("John","Bob","Mary")
+print(names)
+'''
+#Dictionaries
+fav_pizza = {
+	"John": "Pepperoni",
+	"Bob": "Mushroom",
+	"Mary": "Cheese"
+	}
+print (fav_pizza["John"])
+#Boolean
+
+name = True
+
+print (name)
---
+++ variables.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+
+#variables declared
+last_name = "Elder"
+first_name = last_name
+#first_name = "John"
+#print(first_name)
+#first_name = "Bob"
+print(first_name)
+
+#prints out hello world to the screen
+#print("Hello World!")
+
+#prints variable
